644daf7a86df0f30029ee9ceceb3c8633da1756c
misiek1999 <michaldydo99@gmail.com>
2025-09-25 22:34:59 +0200

    Refactor joystick shield code by moving data structures to a separate header file

---
+++ include/joystick_shield_struct.h
@@ -0,0 +1,50 @@
+/**
+ * @file joystick_shield_struct.h
+ * @brief Data structures for joystick shield
+ */
+
+#ifndef JOYSTICK_SHIELD_STRUCT_H
+#define JOYSTICK_SHIELD_STRUCT_H
+
+// Direction enumeration
+enum class JoystickDirection {
+    kLeft,
+    kRight,
+    kUp,
+    kDown,
+    kCenter,
+    kUnknown
+};
+
+struct CalibrationData {
+    int x_center;
+    int y_center;
+    int x_min;
+    int x_max;
+    int y_min;
+    int y_max;
+    bool calibrated;
+};
+
+// Button state structure
+struct ButtonStates {
+    bool button_a;
+    bool button_b;
+    bool button_c;
+    bool button_d;
+    bool button_e;
+    bool button_f;
+    bool joystick_button;
+};
+
+// Joystick data structure
+struct JoystickData {
+    int x_raw;
+    int y_raw;
+    int x_calibrated;
+    int y_calibrated;
+    JoystickDirection x_direction;
+    JoystickDirection y_direction;
+};
+
+#endif // JOYSTICK_SHIELD_STRUCT_H
